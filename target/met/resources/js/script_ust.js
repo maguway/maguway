6af2f4023fb2e92779311122387ab6c877efc7eb
maguway <maguway@gmail.com>
2018-03-31 14:06:52 +0300

    empty

--- target/met/resources/js/script_ust.js
+++
@@ -1,6 +0,0 @@
-// interval is in milliseconds. 1000 = 1 second -> so 1000 * 10 = 10 seconds
-$('.carousel').carousel({
-    interval: 3000,         // интервал
-    keyboard: true,         // переключение кнопками,               default: true
-    wrap: true             // после прохождения цикла - стоп,      default: true
-});
